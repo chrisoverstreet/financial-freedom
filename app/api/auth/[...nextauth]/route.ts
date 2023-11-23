d2ce2a7ded7e20b939cc769e2aa92bf424b5f616
Christopher Overstreet <christopheroverstreet@gmail.com>
2023-11-22 21:41:37 -0500

    🧹🧹🧹

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { authOptions } from '@/lib/next-auth';
 import NextAuth from 'next-auth';
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
